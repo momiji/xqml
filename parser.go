ec3ca52d72d33d382f047914f1854751a195e8dd
xtian <xtian>
2023-06-07 13:02:51 +0200

    optimize objects creation and fix #text slices issue

--- parser.go
+++ parser.go
@@ -13,6 +13,7 @@ type elem struct {
 	name    string
 	path    string
 	isValue bool
+	isEmpty bool
 }
 
 func (x *Xqml) parse(curr *elem, parent *elem, cast bool) error {
@@ -34,24 +35,25 @@ func (x *Xqml) parse(curr *elem, parent *elem, cast bool) error {
 				return fmt.Errorf("Invalid XML element '%s' found for non-partial parse", e.Name.Local)
 			}
 			// create new element
-			data := make(map[string]any)
+			var data map[string]any
 			name := newName(x.namespace, &e.Name)
 			path := newPath(curr.path, name)
-			item := &elem{data, name, path, false}
-			// add attributes
+			item := &elem{data, name, path, false, true}
+			// read attributes
 			if x.attributes {
+				if len(e.Attr) > 0 {
+					data = make(map[string]any)
+					item.data = data
+					item.isEmpty = false
+				}
 				for _, attr := range e.Attr {
 					data["@"+newName(x.namespace, &attr.Name)] = attr.Value
 				}
 			}
-			// upgrade parent if it is a value
-			if curr.isValue {
-				curr.data["#text"] = parent.data[curr.name]
-				curr.isValue = false
-				parent.data[curr.name] = curr.data
-			}
+			// upgrade parent if it is empty or a value
+			x.upgradeValue(curr, parent)
 			// set value
-			x.setValue(curr, name, data)
+			x.addValue(curr, name, path, data)
 			// recursive call
 			err = x.parse(item, curr, cast)
 			if err != nil {
@@ -65,10 +67,6 @@ func (x *Xqml) parse(curr *elem, parent *elem, cast bool) error {
 				}
 			}
 		case xml.EndElement:
-			// if current is empty, downgrade parent
-			if !curr.isValue && len(curr.data) == 0 {
-				x.downgrade(curr, parent, nil)
-			}
 			return nil
 		case xml.CharData:
 			cdata := string(token.(xml.CharData))
@@ -77,23 +75,42 @@ func (x *Xqml) parse(curr *elem, parent *elem, cast bool) error {
 				if x.done {
 					return fmt.Errorf("Invalid XML chardata '%s' found for non-partial parse", cdata)
 				}
+				value := any(cdata)
 				if cast {
-					x.setText(curr, parent, castValue(cdata))
-				} else {
-					x.setText(curr, parent, cdata)
+					value = castValue(cdata)
 				}
+				x.setText(curr, parent, value)
 			}
 		}
 	}
 }
 
-func (x *Xqml) setValue(item *elem, name string, value any) {
+func (x *Xqml) setValue(item *elem, name string, path string, value any) {
 	// if value already set => transform to slice or append to slice
 	if data, ok := item.data[name]; ok {
 		switch data.(type) {
 		case []any:
 			slice := data.([]any)
-			item.data[name] = append(slice, value)
+			slice[len(slice)-1] = value
+			return
+		}
+	}
+	// set value or slice if forced
+	if x.forceList[name] == true || x.forceList[path] == true {
+		item.data[name] = []any{value}
+	} else {
+		item.data[name] = value
+	}
+}
+
+func (x *Xqml) addValue(item *elem, name string, path string, value any) {
+	// if value already set => transform to slice or append to slice
+	if data, ok := item.data[name]; ok {
+		switch data.(type) {
+		case []any:
+			slice := data.([]any)
+			slice = append(slice, value)
+			item.data[name] = slice
 			return
 		default:
 			item.data[name] = []any{data, value}
@@ -101,7 +118,7 @@ func (x *Xqml) setValue(item *elem, name string, value any) {
 		}
 	}
 	// set value or slice if forced
-	if x.forceList[name] == true || x.forceList[item.path] == true {
+	if x.forceList[name] == true || x.forceList[path] == true {
 		item.data[name] = []any{value}
 	} else {
 		item.data[name] = value
@@ -109,9 +126,10 @@ func (x *Xqml) setValue(item *elem, name string, value any) {
 }
 
 func (x *Xqml) setText(curr *elem, parent *elem, value any) {
-	// if curr is empty => downgrade parent
-	if len(curr.data) == 0 {
-		x.downgrade(curr, parent, value)
+	// if curr is empty, set value on parent
+	if curr.isEmpty {
+		x.setValue(parent, curr.name, curr.path, value)
+		curr.isValue = true
 		return
 	}
 	// set value
@@ -125,18 +143,31 @@ func (x *Xqml) setText(curr *elem, parent *elem, value any) {
 	curr.data["#text"] = value
 }
 
-func (x *Xqml) downgrade(curr *elem, parent *elem, value any) {
-	curr.isValue = true
-	data := parent.data[curr.name]
-	switch data.(type) {
-	case []any:
-		slice := data.([]any)
-		slice[len(slice)-1] = value
-	default:
-		if x.forceList[curr.name] == true || x.forceList[curr.path] == true {
-			parent.data[curr.name] = []any{value}
-		} else {
-			parent.data[curr.name] = value
+func (x *Xqml) upgradeValue(curr *elem, parent *elem) {
+	if curr.isEmpty {
+		curr.data = make(map[string]any)
+		curr.isEmpty = false
+		if !curr.isValue {
+			parent.data[curr.name] = curr.data
+		}
+	}
+	if curr.isValue {
+		switch parent.data[curr.name].(type) {
+		case []any:
+			values := parent.data[curr.name].([]any)
+			items := make([]any, len(values))
+			var last map[string]any
+			for i, v := range values {
+				last = map[string]any{"#text": v}
+				items[i] = last
+			}
+			curr.data = last
+			curr.isValue = false
+			parent.data[curr.name] = items
+		default:
+			curr.data["#text"] = parent.data[curr.name]
+			curr.isValue = false
+			parent.data[curr.name] = curr.data
 		}
 	}
 }
